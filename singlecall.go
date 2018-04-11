8c1118c8034c7906a80f9d268bff2916dbcedb4a
llitfkitfk <llitfkitfk@gmail.com>
2018-04-12 00:09:14 +0800

    add single call

---
+++ singlecall.go
@@ -0,0 +1,42 @@
+package singlecall
+
+import "sync"
+
+type call struct {
+	wg  sync.WaitGroup
+	val interface{}
+	err error
+}
+
+type Group struct {
+	mu sync.Mutex
+	m  map[string]*call
+}
+
+// Do 执行function并返回结果，确保每个key只有一个执行
+func (g *Group) Do(key string, fn func() (interface{}, error)) (interface{}, error) {
+	g.mu.Lock()
+
+	if g.m == nil {
+		g.m = make(map[string]*call)
+	}
+
+	if c, ok := g.m[key]; ok {
+		g.mu.Unlock()
+		c.wg.Wait()
+		return c.val, c.err
+	}
+	c := new(call)
+	c.wg.Add(1)
+	g.m[key] = c
+	g.mu.Unlock()
+
+	c.val, c.err = fn()
+	c.wg.Done()
+
+	g.mu.Lock()
+	delete(g.m, key)
+	g.mu.Unlock()
+
+	return c.val, c.err
+}
